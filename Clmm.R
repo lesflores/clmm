6c7de0671c2b9abac3efb98d8ad44db8a639fec1
Lesly Estefanía Flores Rivera <147342430+lesflores@users.noreply.github.com>
2025-04-23 00:00:36 -0600

    Update Clmm.R

--- Clmm.R
+++ Clmm.R
@@ -7,47 +7,6 @@
 # "Neutral", "Satisfecho/a", "Muy satisfecho/a".
 #--------------------
 
-# Variable dependiente
-# "Satisfacción con la democracia" (ordinal)
-# 1 = Muy insatisfecho/a
-# 2 = Insatisfecho/a
-# 3 = Neutral
-# 4 = Satisfecho/a
-# 5 = Muy satisfecho/a
-
-# Variables independientes
-# 1. Confianza en las instituciones (ordinal)
-# Medida en una escala de Likert:
-# 1 = Ninguna confianza
-# 2 = Poca
-# 3 = Algo 
-# 4 = Mucha
-
-# 2. Participación política (binaria)
-# 0 = No ha participado en act políticas
-# 1 = Ha participado en al menos una act política (voto, manifestaciones, pertenencia a partidos, etc.)
-
-# 3. Percepción de la corrupción (ordinal)
-# 1 = Mucha corrupción
-# 2 = Algo
-# 3 = Poca
-# 4 = Ninguna
-
-# 4. Ideología política (categórica)
-# Escala del 1 al 10, donde 1 es extrema izquierda y 10 es extrema derecha.
-
-# 5. Percepción de exclusión o discriminación (binaria)
-# 0 = No ha sufrido exclusión o discriminación
-# 1 = Ha sufrido exclusión o discriminación en alguna ocasión
-
-# 6. Frecuencia de consumo de noticias políticas (ordinal)
-# 1 = Nunca
-# 2 = Rara vez
-# 3 = A veces
-# 4 = Frecuentemente
-# 5 = Todos los días
-
-#--------------------
 # Modelo clmm:
 
 library(readxl)
